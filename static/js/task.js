dc3f0a622acb09669778bb888be33632a1758b1b
sweetticket <yk494@cornell.edu>
2014-06-30 22:14:36 +0900

    starting actual implementation

--- static/js/task.js
+++ static/js/task.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var TIME_INT = 1200;
 	var US_SIZE = 16;
 	var CS_SIZE = 48;
-	
+	var PROXIMITY = "   "; // pixels? spaces?
 	var TARGET = "looking"; // size? in pair? by itself?
 	var US_POS_LIST = ["useful",
 	       "calming",
@@ -60,7 +60,14 @@ $(document).ready(function(){
 		       "NS10",
 		       "NS11",
 		       "NS12"];
-
+	var WINDOW_WIDTH = $(window).width();
+	var WINDOW_HEIGHT = $(window).height();
+	
+	/** Point class */
+	function Point(x,y) {
+		this.x = x || 0;
+		this.y = y || 0;
+	};
 	
 	// set random position random int generating function + offset()
 	// get random coord pair: get screen dimensions, get random number in that range
@@ -68,8 +75,13 @@ $(document).ready(function(){
 	// newPos.left = "randnum1";  // (first in pair)
 	// newPos.top = "randnum2"; // (second in pair)
 	// $("p").offset(newPos);
+	function randomPos(){
+		x = Math.floor(Math.random() * WINDOW_WIDTH);
+		y = Math.floor(Math.random() * WINDOW_HEIGHT);
+		return new Point(x, y);
+	};
 	
-	// central function:
+	/** Running conditions */
 	// params: condition (0,1,2)
 	// CSpos; // cond1: walking, cond2: hearing
 	// CSneut; // cond1: hearing, cond2: walking
@@ -78,32 +90,43 @@ $(document).ready(function(){
 	// 2. randomize lists
 	// 3. preset random coords
 	// 4. set onKeyDown listeners
+	function runPractice(){
+		targetCount = 2; // may not be necessary
+		CSPosCount = 5;
+		CSNeutCount = 5;
+		CSPos = "walking"; // ???
+		CSNeut = "hearing";
+		
+	};
 	
-	var c = 0;
-	var u = 0;
-	var k = 0;
-	var hide1 = Math.random() < .5 ? "#cs" : "#us";
-	var hide2 = hide1 == "#cs" ? "#us" : "#cs";
 	
-	var flashing = setInterval(function(){
-		$("#cs").text(CSlist[c]);
-		$("#us").text(USlist[c][u]);
-		$(hide1).show(0).delay(275).hide(0).delay(25).show(0);
-		$(hide2).show(0).delay(575).hide(0).delay(25).show(0);
-		if (k == 4) {
-			k = 0;
-			u++;
-		}
-		if (u >= USlist[c].length) {
-			u = 0;
-			c++;
-		}
-		if (c >= CSlist.length ) {
-			clearInterval(flashing);
-		}
-		k++;
-
-	}, 600);
+	
+	
+	//var c = 0;
+	//var u = 0;
+	//var k = 0;
+	//var hide1 = Math.random() < .5 ? "#cs" : "#us";
+	//var hide2 = hide1 == "#cs" ? "#us" : "#cs";
+	//
+	//var flashing = setInterval(function(){
+	//	$("#cs").text(CSlist[c]);
+	//	$("#us").text(USlist[c][u]);
+	//	$(hide1).show(0).delay(275).hide(0).delay(25).show(0);
+	//	$(hide2).show(0).delay(575).hide(0).delay(25).show(0);
+	//	if (k == 4) {
+	//		k = 0;
+	//		u++;
+	//	}
+	//	if (u >= USlist[c].length) {
+	//		u = 0;
+	//		c++;
+	//	}
+	//	if (c >= CSlist.length ) {
+	//		clearInterval(flashing);
+	//	}
+	//	k++;
+	//
+	//}, 600);
 	
 
 });
