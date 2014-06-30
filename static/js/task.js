7e20dd956c9f605f5f02f887bbcb1ccd3b1d3c89
sweetticket <yk494@cornell.edu>
2014-06-30 22:44:46 +0900

    runTrial

--- static/js/task.js
+++ static/js/task.js
@@ -81,6 +81,21 @@ $(document).ready(function(){
 		return new Point(x, y);
 	};
 	
+	/** Show a pair or single word */
+	// left = word on left
+	// right = word on right (may be null)
+	// position = position relative to viewport
+	function runTrial(word1, word2){
+		position = randomPos();
+		if (word2 == null) {
+			trialClass = Math.random > .5 ? "CS" : "US"; // MAY CHANGE (this is to match this to an element in css file, for SIZING)
+			// add paragraph node
+		}else{
+			// somehow break up the two parts so that they have different classes
+			// ADD A PARAM SO THAT WE KNOW IF THE WORD IS US, CS, NS
+		}
+	};
+	
 	/** Running conditions */
 	// params: condition (0,1,2)
 	// CSpos; // cond1: walking, cond2: hearing
